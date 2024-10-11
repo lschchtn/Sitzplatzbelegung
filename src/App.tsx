e4d5178f5f174d70e469ab18944d6e965dddd3ac
lschchtn <letizia.schachten@monday-consulting.com>
2024-10-11 12:55:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Seat, type Seats  } from "./components/Seat.tsx";
-import data from "./data.json";
-console.log(data);
+import { Seat  } from "./components/Seat.tsx";
 
-const seats = data.seats;
+let obj;
+const res = await fetch('http://192.168.109.99:8081/frontend')
+obj = await res.json();
+const json = JSON.parse(JSON.parse(obj));
+console.log(json);
+
+const seats = json.Seats;
 
 function App() {
   return (
     <>
       <h1 className="text-center text-xl my-5">Wagon 1</h1>
       <div className="mx-auto my-5 h-full w-1/3 grid grid-cols-4 gap-3 space-x-0">
-        {seats.map((seat) => <Seat id={seat.id} taken={seat.taken} />)}
+        {seats.map((seat) => <Seat key={seat.id} id={seat.id} taken={seat.taken} />)}
       </div>
     </>
   )
